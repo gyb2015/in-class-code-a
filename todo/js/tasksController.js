c15d3a9249cfec3923410b7994b02cc392b823d2
unknown <iguest@is-m440m27-15.ischool.uw.edu>
2013-12-13 17:55:27 -0800

    new files.

--- todo/js/tasksController.js
+++ todo/js/tasksController.js
@@ -5,3 +5,24 @@
     TasksListView, and NewTaskView into a working application
 */
 
+$(function(){
+	var tasks = createTasks({
+		tasks: [{title: 'My First Task', done: false}]
+	});
+
+	var tasksView = createTasksListView({
+		model: tasks,
+		template: $('.tasks-template'),
+		container: $('.tasks-container')
+	});
+
+	var newTaskView = createNewTaskView({
+		model: tasks,
+		form: $('.new-task-form')
+	});
+
+	$('.btn-remove-done').click(function(){
+		tasks.removeDone();
+	});
+
+});
